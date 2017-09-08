67d1a1c797500b922afc10371dfa8e22cdf2d4a6
Josiah Horton <joehorton99@gmail.com>
2017-09-07 17:11:32 -0700

    Convert controller to kotlin

---
+++ app/src/main/java/com/jtechme/jumpgo/controller/UIController.kt
@@ -0,0 +1,91 @@
+package com.jtechme.jumpgo.controller
+
+import com.jtechme.jumpgo.browser.TabsManager
+import com.jtechme.jumpgo.database.HistoryItem
+import com.jtechme.jumpgo.dialog.LightningDialogBuilder
+import com.jtechme.jumpgo.view.LightningView
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Message
+import android.support.annotation.ColorInt
+import android.view.View
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient.CustomViewCallback
+
+interface UIController {
+
+    fun changeToolbarBackground(favicon: Bitmap, drawable: Drawable?)
+
+    @get:ColorInt
+    val uiColor: Int
+
+    val useDarkTheme: Boolean
+
+    fun updateUrl(title: String?, isLoading: Boolean)
+
+    fun updateProgress(n: Int)
+
+    fun updateHistory(title: String?, url: String)
+
+    fun openFileChooser(uploadMsg: ValueCallback<Uri>)
+
+    fun onShowCustomView(view: View, callback: CustomViewCallback)
+
+    fun onShowCustomView(view: View, callback: CustomViewCallback, requestedOrienation: Int)
+
+    fun onHideCustomView()
+
+    fun onCreateWindow(resultMsg: Message)
+
+    fun onCloseWindow(view: LightningView)
+
+    fun hideActionBar()
+
+    fun showActionBar()
+
+    fun showFileChooser(filePathCallback: ValueCallback<Array<Uri>>)
+
+    fun closeEmptyTab()
+
+    fun showCloseDialog(position: Int)
+
+    fun newTabButtonClicked()
+
+    fun tabCloseClicked(position: Int)
+
+    fun tabClicked(position: Int)
+
+    fun newTabButtonLongClicked()
+
+    fun bookmarkButtonClicked()
+
+    fun bookmarkItemClicked(item: HistoryItem)
+
+    fun closeBookmarksDrawer()
+
+    fun setForwardButtonEnabled(enabled: Boolean)
+
+    fun setBackButtonEnabled(enabled: Boolean)
+
+    fun tabChanged(tab: LightningView)
+
+    val tabModel: TabsManager
+
+    fun onBackButtonPressed()
+
+    fun onForwardButtonPressed()
+
+    fun onHomeButtonPressed()
+
+    fun handleBookmarksChange()
+
+    fun handleDownloadDeleted()
+
+    fun handleBookmarkDeleted(item: HistoryItem)
+
+    fun handleNewTab(newTabType: LightningDialogBuilder.NewTab, url: String)
+
+    fun handleHistoryChange()
+
+}
