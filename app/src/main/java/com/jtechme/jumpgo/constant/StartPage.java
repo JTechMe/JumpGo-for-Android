d6c75a0ceb1aa3d5e9972c6c4a66256d62cb0862
Josiah Horton <joehorton99@gmail.com>
2017-09-15 12:09:16 -0700

    Update homepage search bar to match url bar style

--- app/src/main/java/com/jtechme/jumpgo/constant/StartPage.java
+++ app/src/main/java/com/jtechme/jumpgo/constant/StartPage.java
@@ -39,7 +39,7 @@ public class StartPage {
         + "width:100%;outline:none;border:none;font-size: 16px;background-color:transparent;}"
         + "span { display: block; overflow: hidden; padding-left:5px;vertical-align:middle;}"
         + ".search_bar{display:table;vertical-align:middle;width:90%;height:35px;max-width:500px;margin:0 auto;background-color:#fff;box-shadow: 0px 2px 3px rgba( 0, 0, 0, 0.25 );"
-        + "font-family: Arial;color: #444;-moz-border-radius: 2px;-webkit-border-radius: 2px;border-radius: 2px;}"
+        + "font-family: Arial;color: #444;-moz-border-radius: 20px;-webkit-border-radius: 20px;border-radius: 20px;}"
         + "#search_submit{outline:none;height:37px;float:right;color:#404040;font-size:16px;font-weight:bold;border:none;"
         + "background-color:transparent;}.outer { display: table; position: absolute; height: 100%; width: 100%;}"
         + ".middle { display: table-cell; vertical-align: middle;}.inner { margin-left: auto; margin-right: auto; "
@@ -47,7 +47,7 @@ public class StartPage {
         + ".box { vertical-align:middle;position:relative; display: block; margin: 10px;padding-left:10px;padding-right:10px;padding-top:5px;padding-bottom:5px;"
         + " background-color:#fff;box-shadow: 0px 3px rgba( 0, 0, 0, 0.1 );font-family: Arial;color: #444;"
         + "font-size: 12px;-moz-border-radius: 2px;-webkit-border-radius: 2px;"
-        + "border-radius: 2px;}</style><body> <div class=\"outer\"><div class=\"middle\"><div class=\"inner\"><img class=\"smaller\" src=\"";
+        + "border-radius: 20px;}</style><body> <div class=\"outer\"><div class=\"middle\"><div class=\"inner\"><img class=\"smaller\" src=\"";
 
     private static final String MIDDLE = "\" ></br></br><form onsubmit=\"return search()\" class=\"search_bar\" autocomplete=\"off\">"
         + "<input type=\"submit\" id=\"search_submit\" value=\"Search\" ><span><input class=\"search\" type=\"text\" value=\"\" id=\"search_input\" >"
